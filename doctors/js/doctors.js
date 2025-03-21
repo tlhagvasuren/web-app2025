a357f9e9a5b226cea5e6d6dc0cf7459843b736f0
lkhagvasuren <lhagvaatuvshin@gmail.com>
2025-03-21 10:14:29 +0800

    jagsaalt2

--- doctors/js/doctors.js
+++ doctors/js/doctors.js
@@ -19,4 +19,14 @@ $(document).ready(function(){
 
     })
 
-});
+}); 
+
+function openModal(title, content) {
+    document.getElementById("garchig").innerText = title;
+    document.getElementById("medeelel").innerText = content;
+    document.getElementById("modalDavharga").classList.add("active");
+}
+
+function closeModal() {
+    document.getElementById("modalDavharga").classList.remove("active");
+}
--- services/js/index.js
+++ services/js/index.js
@@ -1,14 +1,17 @@
-function openModal(title, content) {
-    document.getElementById("garchig").innerText = title;
-    document.getElementById("medeelel").innerText = content;
-    document.getElementById("modalDavharga").classList.add("active");
-}
+const acc = document.querySelectorAll(".accordion");
+    acc.forEach(button => {
+        button.addEventListener("click", function() {
+            this.classList.toggle("active");
+            const panel = this.nextElementSibling;
+            if (panel.style.maxHeight) {
+                panel.style.maxHeight = null;
+            } else {
+                panel.style.maxHeight = panel.scrollHeight + 50 + "px";
+                console.log(panel.scrollHeight);
+            }
+        });
+    });
 
-function closeModal() {
-    document.getElementById("modalDavharga").classList.remove("active");
-}
-
-// 
 $(document).ready(function(){
 
     $('.fa-bars').click(function(){
